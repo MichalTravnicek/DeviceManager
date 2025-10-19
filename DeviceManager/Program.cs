42ab72e05a20073a715007562cb8df8bd9a5987c
Michal Travnicek <177191047+MichalTravnicek@users.noreply.github.com>
2025-10-19 12:23:07 +0200

    Blank main class

---
+++ DeviceManager/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Threading.Channels;
+
+namespace DeviceManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Device Manager started.");
+            Console.ReadKey();
+        }
+    }
+}
